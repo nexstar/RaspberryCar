5c1913917d2c96421899d61c1128664ed92fb732
nexstar <nexstar1436@gmail.com>
2016-07-10 16:14:55 +0800

    Apach&PHP

---
+++ Apache&PHP/Apach&PHP.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+#aothor:Nianbao
+
+echo "******install-Apache******"
+sudo apt-get -y install apache2
+echo "Create html with myself(Nianbao)"
+sudo vim /var/www/html/Nianbao.html << <html><p>123123</p></html>
+
+echo "******install-Apache******"
