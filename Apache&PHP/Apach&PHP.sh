066003988d2a73d868ec563c253c433d5e7c2b89
nexstar <nexstar1436@gmail.com>
2016-09-17 06:16:45 +0800

    all

--- Apache&PHP/Apach&PHP.sh
+++ Apache&PHP/Apach&PHP.sh
@@ -5,32 +5,21 @@ echo "input user"
 read user
 
 echo "******install-Apache******"
-sudo apt-get install apache2 -y
+sudo apt-get install apache2
 echo "Create html like test.html"
-sudo chown -R $user:$user /var/www/
-cd /var/www/html && echo "<html> <p>123</p> <html/>" >> test.html
+sudo chown -R $user:$user /var/www/html/
 
 echo "******install-PHP******"
-sudo apt-get install php5 libapache2-mod-php5 -y
+sudo apt-get install php5 libapache2-mod-php5
 systemctl restart apache2
 echo "<?php phpinfo(); ?>" >> info.php
 
 #echo "*****Get MySQL********"
-#sudo apt-cache search php5 -y
+sudo apt-cache search php5
+sudo apt-get install php5-mysqlnd php5-curl php5-gd php5-intl php-pear php5-imagick php5-imap php5-mcrypt php5-memcache php5-ming php5-ps php5-pspell php5-recode php5-snmp php5-sqlite php5-tidy php5-xmlrpc php5-xsl
+sudo systemctl restart apache2
 
-#sudo apt-get install php5-mysqlnd php5-curl php5-gd php5-intl php-pear php5-imagick php5-imap php5-mcrypt php5-memcache php5-ming php5-ps php5-pspell php5-recode php5-snmp php5-sqlite php5-tidy php5-xmlrpc php5-xsl -y
-
-#sudo systemctl restart apache2
-echo "***Write Video******"
-sudo apt-get install git -y
-#git clone https://github.com/nexstar/RaspberryCar.git
-cd $HOME
-sudo cp ~/RaspberryCar/Apache\&PHP/Video.html /var/www/html/
-
-echo "Check apache, html, php and video, if have problem can try more browser."
-echo "Check apache please input http://127.0.0.1/"
-echo "Check html please input http://127.0.0.1/test.html "
-echo "Check php please input http://127.0.0.1/info.php"
-echo "Check video please input http://127.0.0.1/Video.html"
-
-
+#echo "******phpmyadmin**********"
+sudo apt-get install phpmyadmin
+cd && cd /var/www/html
+sudo ln -s /usr/share/phpmyadmin
